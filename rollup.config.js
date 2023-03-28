54d7a487db4ccc987913a34f278d91e08f7881df
Quincy Qiu <qiuqfang@outlook.com>
2023-03-28 14:06:49 +0800

    chore: update lint and format

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ import json from "@rollup/plugin-json";
 export default {
   input: "./src/index.ts",
   output: [
-    { file: "dist/index.js", format: "cjs" },
+    { file: "dist/index.cjs", format: "cjs" },
     { file: "dist/index.mjs", format: "es" },
   ],
 
