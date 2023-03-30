fcd9cc8d64d39fc7995cb50e948b3ede1697388a
Quincy Qiu <qiuqfang@outlook.com>
2023-03-30 11:55:25 +0800

    chore: babel update swc

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@
 
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
 import del from "rollup-plugin-delete";
+import swc from "@qiuqfang/rollup-plugin-swc";
 
 export default {
   input: "./src/index.ts",
@@ -15,11 +14,5 @@ export default {
     { file: "dist/index.mjs", format: "es" },
   ],
 
-  plugins: [
-    babel({ babelHelpers: "bundled", extensions: [".ts"] }),
-    resolve({ extensions: [".ts"] }),
-    commonjs(),
-    json(),
-    del({ targets: "dist/*" }),
-  ],
+  plugins: [swc(), commonjs(), json(), del({ targets: "dist/*" })],
 };
